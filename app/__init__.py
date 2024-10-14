5cc20d21b92b5606b02bfee3f751fdb4f23eee65
Hieu <153698628+hieuchaydi@users.noreply.github.com>
2024-10-14 21:04:19 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,12 +8,9 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     
-    # Cấu hình ứng dụng
-    app.config['SECRET_KEY'] = 'b5d4f2c7e1e2c6d93c8b7d5b9e3f71a2c6d4b5e8f7d6a3b8e4e1b5d4a7f8c6d9a'
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user.db"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.permanent_session_lifetime = timedelta(days=5)
-    
+    # Cấu hình ứng dụng từ tệp config.py
+    app.config.from_object('config.Config')
+
     # Khởi tạo các thành phần
     db.init_app(app)
 
