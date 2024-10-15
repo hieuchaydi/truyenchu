d903cc236e95ab302f5e8302817005579ab14dd0
Hieu <153698628+hieuchaydi@users.noreply.github.com>
2024-10-15 19:17:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     
     with app.app_context():
         db.create_all()  # Tạo tất cả các bảng trong cơ sở dữ liệu
-    app.run(debug=False)
+    app.run(debug=True)
 #gunicorn 'app:create_app()' --bind 0.0.0.0:$PORT w
